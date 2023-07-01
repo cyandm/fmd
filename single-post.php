fa2600e21a00385be7a0add2e9d6321afb1adf64
amirtanazzoh <92968872+amirtanazzoh@users.noreply.github.com>
2023-07-01 12:36:12 +0330

    single-blog-ui Responsive

---
+++ single-post.php
@@ -0,0 +1,86 @@
+<?php get_header( null, [ 'border' => true, 'preloader' => false ] ) ?>
+
+<main class="single-post container">
+	<?php get_template_part( 'templates/parts/sidebar', 'blog' ) ?>
+
+	<section class="main-content">
+		<div class="post-wrapper">
+			<div class="post-info">
+				<a href="" class="primary-btn">
+					<i class="icon-arrow-long-left"></i>
+				</a>
+				<?= wp_get_attachment_image( 55, 'full', false, [ 'class' => 'feature-image' ] ) ?>
+				<div class="title">
+					<h1>
+						<?= get_the_title() ?>
+					</h1>
+				</div>
+				<div class="post-meta">
+					<div class="post-meta-inner-wrapper">
+						<span>Author : </span>
+						<span>
+							<?= get_the_author() ?>
+						</span>
+					</div>
+					<div class="post-meta-inner-wrapper">
+						<span>Date : </span>
+						<span>
+							<?= get_the_date() ?>
+						</span>
+					</div>
+				</div>
+			</div>
+			<div class="post-content">
+				<?= get_the_content() ?>
+			</div>
+		</div>
+
+		<div class="related-blogs-container">
+			<h2>Related Blogs</h2>
+			<div class="related-blogs-wrapper">
+				<div class="blog-item card">
+					<?= wp_get_attachment_image( 55, 'full', false, [ 'class' => 'hero-blog cyn-slide active' ] ) ?>
+					<div class="card-information">
+						<div>
+							<p class="card-title">Columns</p>
+							<p class="card-description"> 140 items</p>
+						</div>
+
+						<a href="#">
+							<i class="icon-arrow-down-right"></i>
+						</a>
+					</div>
+				</div>
+				<div class="blog-item card">
+					<?= wp_get_attachment_image( 55, 'full', false, [ 'class' => 'hero-blog cyn-slide active' ] ) ?>
+					<div class="card-information">
+						<div>
+							<p class="card-title">Columns</p>
+							<p class="card-description"> 140 items</p>
+						</div>
+
+						<a href="#">
+							<i class="icon-arrow-down-right"></i>
+						</a>
+					</div>
+				</div>
+				<div class="blog-item card">
+					<?= wp_get_attachment_image( 55, 'full', false, [ 'class' => 'hero-blog cyn-slide active' ] ) ?>
+					<div class="card-information">
+						<div>
+							<p class="card-title">Columns</p>
+							<p class="card-description"> 140 items</p>
+						</div>
+
+						<a href="#">
+							<i class="icon-arrow-down-right"></i>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</section>
+</main>
+
+<?php get_footer() ?>
---
+++ templates/parts/sidebar-blog.php
@@ -0,0 +1,75 @@
+<aside class="sidebar-blog">
+	<div class="search-container">
+		<input type="search" class="search" placeholder="search">
+		<i class="icon-search"></i>
+	</div>
+
+	<div class="categories-container">
+		<h3 class="sidebar-title">Categories</h3>
+		<div class="categories-wrapper desktop">
+			<div class="category-info active">
+				<span class="category-name">All</span>
+				<span class="category-count">209</span>
+			</div>
+			<div class="category-info">
+				<span class="category-name">to know more</span>
+				<span class="category-count">124</span>
+			</div>
+			<div class="category-info">
+				<span class="category-name">interior</span>
+				<span class="category-count">168</span>
+			</div>
+			<div class="category-info">
+				<span class="category-name">DIY</span>
+				<span class="category-count">150</span>
+			</div>
+
+
+		</div>
+		<div class="categories-wrapper mobile">
+			<div class="category-info">
+				<span class="category-name">to know more</span>
+				<i class="icon-arrow-down"></i>
+			</div>
+		</div>
+	</div>
+
+	<div class="recommend-container">
+
+		<h3 class="sidebar-title">recommended</h3>
+
+		<div class="recommend-wrapper">
+			<div class="recommend-item">
+				<?= wp_get_attachment_image( 55, 'full', false, [ 'class' => '' ] ) ?>
+				<div class="info">
+					<span class="title">Decide About Wall Colors</span>
+					<span class="date">2023/05/23</span>
+				</div>
+			</div>
+
+			<div class="recommend-item">
+				<?= wp_get_attachment_image( 56, 'full', false, [ 'class' => '' ] ) ?>
+				<div class="info">
+					<span class="title">Decide About Wall Colors</span>
+					<span class="date">2023/05/23</span>
+				</div>
+			</div>
+
+			<div class="recommend-item">
+				<?= wp_get_attachment_image( 55, 'full', false, [ 'class' => '' ] ) ?>
+				<div class="info">
+					<span class="title">Decide About Wall Colors</span>
+					<span class="date">2023/05/23</span>
+				</div>
+			</div>
+
+			<div class="recommend-item">
+				<?= wp_get_attachment_image( 56, 'full', false, [ 'class' => '' ] ) ?>
+				<div class="info">
+					<span class="title">Decide About Wall Colors</span>
+					<span class="date">2023/05/23</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</aside>
